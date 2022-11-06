31a0908dccdcf08a2ea21f72c15d76de4765efb1
Antonio Pereira <anthonyesany0712@gmail.com>
2022-11-06 17:54:15 -0300

    feat: criação de titulo variavel

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard title="Meu Painel de Controle" />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
